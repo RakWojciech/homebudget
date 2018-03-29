042a648905131f9681f18553af053aba390b8ba3
Wojtek Rak <wojtek.rak96@gmail.com>
2018-03-30 00:56:16 +0200

    fix

--- src/app/view/pay/pay.component.ts
+++ src/app/view/pay/pay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {RouterModule, Router, NavigationEnd, NavigationStart, ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -13,6 +13,15 @@ export class PayComponent implements OnInit {
 	car;
 	shop;
 	msg;
+	//
+	@ViewChild('date') date: any;
+	@ViewChild('payout') payout: any;
+	@ViewChild('price') price: any;
+	@ViewChild('category') category: any;
+	//
+	// storageObj;
+	payment;
+	categoryList;
 	constructor(private router: Router) {
 		// this.router.events.subscribe(event=>{
 		// 	if(event instanceof NavigationEnd) {
@@ -33,6 +42,25 @@ export class PayComponent implements OnInit {
 		this.rent = localStorage.getItem("rent");
 		this.car = localStorage.getItem("car");
 		this.shop = localStorage.getItem("shop");
+		this.payment = localStorage.getItem("payment");
+		this.categoryList = localStorage.getItem("category");
 	}
-
+	addPayment() {
+		var allPayment = JSON.parse(localStorage.getItem('payment')) || [];
+		let storageObj = {
+			'date': this.date.nativeElement.value,
+			'payout': this.payout.nativeElement.value,
+			'price': this.price.nativeElement.value,
+		}
+		allPayment.push(storageObj);
+		localStorage.setItem('payment', JSON.stringify(allPayment));
+	}
+	categoryAdd() {
+		var allCategory = JSON.parse(localStorage.getItem('allCategoryArray')) || [];
+		let storageObj = {
+			'category-name': this.category.nativeElement.value
+		}
+		allCategory.push(storageObj);
+		localStorage.setItem('category', JSON.stringify(allCategory));
+	}
 }
